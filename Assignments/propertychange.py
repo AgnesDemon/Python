95e16f360bee1ec1f1ca33a89e05786be9cbc36a
Chavez <chavelina0105@gmail.com>
2022-11-09 16:33:26 -0600

    saving changes

--- Assignments/propertychange.py
+++ Assignments/propertychange.py
@@ -18,5 +18,19 @@ Which property would you like to change?
 loop until user is satisfied
 
 '''
+#My 3 properties: Strength +15, Resistance to All Elements +20%, Critical Hit Chance +12%
+
+from random import randint
+
+
+properties = { 1 : "Intelligence +30%)", 2 : "Fire Resistance +12%", 3 : "Critical Hit Damage +5%", 4 : "Critical Hit Chance +15%", 5 : "Strength +11"}
+
+print("Change Property")
+changeProperty = input()
+randomProperty = randint(1,5)
+print(properties[randomProperty])
+
+
+
 
 
