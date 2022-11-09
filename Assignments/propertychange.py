f9c4fcc0037289723518ac7c5b5ff1ac3ab80302
Chavez <chavelina0105@gmail.com>
2022-11-08 20:54:49 -0600

    saving property assignment

---
+++ Assignments/propertychange.py
@@ -0,0 +1,22 @@
+#Assignment
+#Display three properties
+#Allow user to change one of the properties using the key
+#System will generate a random property and display 3 properties again
+'''
+initial properties will be randomly generated
+1 strength
+2 armor
+3 resistance
+
+Which property would you like to change?
+1
+
+1 life steal
+2 armor
+3 resistance
+
+loop until user is satisfied
+
+'''
+
+
