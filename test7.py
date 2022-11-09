3994697cfe6dc99d881e10bca48f071b182ea9d9
Chavez <chavelina0105@gmail.com>
2022-11-08 20:58:57 -0600

    saving property assignment

--- test7.py
+++ test7.py
@@ -2,6 +2,7 @@
 #Rounding
 number = 105.65
 print(round(number, 0))
+print(round(number, 2))
 
 #exit() is a function that stops a function.
 
@@ -22,3 +23,7 @@ except:
 
 #11-8-2022
 print("Hello.")
+#git status - shows what files have changed
+#git add [file name] - adds files to be committed
+#git commit -m "whatever comments you want to make" - will be visible in the respository
+#git push - sends to the remote respository
