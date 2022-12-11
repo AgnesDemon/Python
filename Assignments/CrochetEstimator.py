e36c594a9be7f2babb2b0126d70a9d3f701f0f6a
Chavez <chavelina0105@gmail.com>
2022-12-10 20:40:11 -0600

    Saving modified data

--- Assignments/CrochetEstimator.py
+++ Assignments/CrochetEstimator.py
@@ -11,3 +11,32 @@ class Crochet:
         this.CrochetTools = tools
         this.Eyes = eyes
         this.Pattern = pattern
+        this.Hours = hours
+    
+    def SupplyCost(this):
+        cost = this.Yarn + this.CrochetTools + this.Eyes + this.Pattern
+        return cost
+    
+    def HourlyCost(this):
+        hourcost = this.Hours * 12
+        return hourcost
+    
+
+crochetBag = Crochet(5, 30, 1, 0, 8)
+hourlyCost = crochetBag.HourlyCost()
+supplyCost = crochetBag.SupplyCost()
+#total = crochetBag.HourlyCost + crochetBag.SupplyCost()
+
+print("Bag:", hourlyCost, ",", supplyCost, ",", hourlyCost + supplyCost)
+#print(hourlyCost)
+#print(supplyCost)
+#print(hourlyCost + supplyCost)
+
+crochetSchnauzer = Crochet(4, 30, 1, 6, 7)
+hourlyCost = crochetSchnauzer.HourlyCost()
+supplyCost = crochetSchnauzer.SupplyCost()
+
+print("Schnauzer:", hourlyCost, ",", supplyCost, ",", hourlyCost + supplyCost)
+#print(hourlyCost)
+#print(supplyCost)
+#print(hourlyCost + supplyCost)
