95096755f1b31742f71ebfa97aad2bc8718074dd
Chavez <chavelina0105@gmail.com>
2023-01-02 20:10:30 -0600

    Saving modified data

---
+++ Assignments/contact_book_project.py
@@ -0,0 +1,31 @@
+#Contact Book Project
+
+#Show total number of contacts, maybe show first x amount.
+#What would you like to do?
+    #Search for contact
+        #Search by first name
+    #Add new contact
+        #Probably allow for blank data
+    #Edit contact
+        #What will the edit screen look like, maybe ask them what they want to edit
+    #Display Contact
+        #Shows all contacts
+            #Allow user to pick a user id to show a specific contact
+    #Remove contact
+        #Probably should confirm they really want to do this
+        #How do we decide who to remove
+
+#Contact:
+    #First name
+    #Last name
+    #Address
+    #City
+    #State
+    #Zip
+    #Email
+    #Phone
+
+#Adding would save the record in some fashion, maybe CSV
+
+#Example: Isabel, Aguillon, 14114 Wetmore Bend, San Antonio, TX, 78247, chavelina0105@gmail.com, 210-725-0262
+
