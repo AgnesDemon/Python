54ce8e333a16a06c3cd25ea10db95e6081d4e023
Chavez <chavelina0105@gmail.com>
2023-10-31 22:58:14 -0500

    Saving modified data

--- packt_python/app_1/app_1.py
+++ packt_python/app_1/app_1.py
@@ -24,12 +24,22 @@ import pandas
 data = pandas.read_csv("volcanoes.csv")
 print(data)
 print("\n")
-print(data["STATE"])
-print("\n")
+#print(data["STATE"])
+#print("\n")
 
 lat = list(data["LATITUDE"])
 lon = list(data["LONGITUDE"])
 name = list(data["NAME"])
+elev = list(data["HEIGHT"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    #elif 1000 <= elevation < 3000:
+        #return 'orange'
+    #else:
+        #return 'red'
+
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
@@ -43,11 +53,9 @@ fg.add_child(folium.Marker(location=[38.7, 99.1], popup="Location2", icon=folium
     fg.add_child(folium.Marker(location=coordinates, popup="Location", icon=folium.Icon(color='green')))'''
 
 #You can use a txt or csv file that holds the data of lat and lon to make locations on the map
-for lt, ln, nm in zip(lat, lon, name):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=nm, icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el), icon=folium.Icon(color=color_producer(el))))
 #If your popup=(parameter) is an integer, type str(parameter) to fix the error
-
-
 
 map.add_child(fg)
 #or map.add_child(folium.Marker(location=[38.2, -99.1], popup="Location1", icon=folium.Icon(color='green')))
