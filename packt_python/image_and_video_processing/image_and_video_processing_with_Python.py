cb14fd124de940648d7e834b4e8a9e83c615c141
Chavez <chavelina0105@gmail.com>
2023-11-30 13:05:26 -0600

    Saving modified data

--- packt_python/image_and_video_processing/image_and_video_processing_with_Python.py
+++ packt_python/image_and_video_processing/image_and_video_processing_with_Python.py
@@ -9,7 +9,7 @@ import cv2
 import glob
 #glob is a library that finds path names of files
 
-'''#img = cv2.imread("galaxy.jpg", 0)
+'''#img = cv2.imread("galaxy.jpg", 0) #0 makes the image black, gray, and white.
 #img = cv2.imread("galaxyjpg", 1)
 img = cv2.imread("galaxy.jpg", -1)
 
@@ -25,7 +25,8 @@ cv2.waitKey(0) #0 will allow you to press any button to close the image.
 #If you type another number, that will be the amount of miliseconds the image will last before closing on its own.
 cv2.destroyAllWindows()'''
 
-images = glob.glob("*.jpg") #looks and makes a list of files that end in .jpg.
+#Shows each image for 500 miliseconds (1/2 second), closes each image, and saves them.
+'''images = glob.glob("*.jpg") #looks and makes a list of files that end in .jpg.
 
 for image in images:
     img = cv2.imread(image,0) #black and white image due to 0.
@@ -34,4 +35,30 @@ for image in images:
     cv2.waitKey(500) #waits 500 miliseconds, or approximately 1/2 a second.
     cv2.destroyAllWindows()
     cv2.imwrite("resized_" + image, re) #renames image to resized_(image name).
+    #Example: resized_galaxy.jpg.'''
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+img = cv2.imread("photo.jpg")
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.05, minNeighbors=5)
+#scaleFactor = 1.05 will decrease the scale by 5%.
+#If you were to put 1.5 in scaleFactor, it would decrease the scale by 50%.
+
+for x, y, w, h in faces:
+    img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    #Draws a square or rectangle that maps out the face.
+
+print(type(faces)) #Prints what class faces is.
+print(faces) #Prints the values that define the face in the image.
+
+resized = cv2.resize(img, (int(img.shape[1]/3), int(img.shape[0]/3)))
+
+cv2.imshow("Gray", img) #Shows image.
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#In cases with images with more than one face or unclear faces, you can adjust the scaleFactor to better find the faces.
+#However, it won't be perfect. There will be some faces that the program just won't be able to detect.
 
