074413f7d6a3b257cc747c751e3b24ddf21f1f08
Chavez <chavelina0105@gmail.com>
2023-12-11 15:11:54 -0600

    Saving modified data

--- packt_python/app_3/controlling_the_webcam_and_detecting_objects.py
+++ packt_python/app_3/controlling_the_webcam_and_detecting_objects.py
@@ -1,44 +1,62 @@
 import cv2
+import time
+from datetime import datetime
 
 first_frame = None
+status_list = [None, None]
+times = []
 
 video = cv2.VideoCapture(0)
 
 while True:
     check, frame = video.read()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    status = 0
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #Changes color frame to gray scale.
+    gray = cv2.GaussianBlur(gray, (21, 21), 0) #Blurs gray image.
     
     if first_frame is None:
-        first_frame = gray #assigns the numpy array to first_frame
+        first_frame = gray #Assigns the numpy array to first_frame
         continue
 
     delta_frame = cv2.absdiff(first_frame, gray)
-
     thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
-
     thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2)
 
     (cnts,_) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 10000:
             continue
+        #If contours found are greater than 10,000, then the status variable changes from 0 to 1, as shown below.
+        status = 1
     
         (x, y, w, h) = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3) #This is for mapping out the face in "Color Frame."
 
-    cv2.imshow("Gray Frame", gray)
-    cv2.imshow("Delta Frame", delta_frame)
-    cv2.imshow("Threshold Frame", thresh_frame)
-    cv2.imshow("Color Frame", frame)
+    status_list.append(status)
+
+    if status_list [-1] == 1 and status_list [-2] == 0:
+        times.append(datetime.now())
+    if status_list [-1] == 0 and status_list [-2] == 1:
+        times.append(datetime.now())
+
+    cv2.imshow("Gray Frame", gray) #Shows gray scale frame.
+    cv2.imshow("Delta Frame", delta_frame) #Shows frame where you appear see-through.
+    cv2.imshow("Threshold Frame", thresh_frame) #Shows the frame where the only colors there are black and white.
+    #Black indicates no movement.
+    #White indicates movement.
+    cv2.imshow("Color Frame", frame) #Shows color frame where your face is inside a green box.
 
     key = cv2.waitKey(1)
-    print(gray)
+    #print(gray)
+    #print(delta_frame)
+    #These two print the numpy arrays.
 
-    if key == ord('q'):
+    if key == ord('q'): #If you press 'q' (not in the terminal or code), the windows should close.
         break
+
+    print(status_list)
+    print(times)
 
 video.release()
 cv2.destroyAllWindows()
