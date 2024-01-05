43666a758d7e98e62b80e00b1ede531c121cefc8
Chavez <chavelina0105@gmail.com>
2024-01-05 12:23:10 -0600

    Saving modified data

--- packt_python/bokeh/interactive_data_visualization_with_python_and_bokeh.py
+++ packt_python/bokeh/interactive_data_visualization_with_python_and_bokeh.py
@@ -1,2 +1,38 @@
 #To open Jupyter Notebook from terminal, type "jupyter notebook".
 #To run something in Jupyter, press Shift + Enter.
+
+#making a basic bokeh line graph
+    #from bokeh.plotting import figure
+    #from bokeh.io import output_file, show
+#preparing data
+    #x = [1, 2, 3]
+    #y = [6, 7, 8]
+#preparing the output file
+    #output_file("Line.html")
+#creating figure object
+    #f = figure()
+#creating line plot
+    #f.line(x,y)
+#writing the plot in the figure object
+    #show(f)
+#This will take the data from the two lists - x and y - and create a line plot with the data in another window.
+
+
+#making a basic bokeh line graph
+    #from bokeh.plotting import figure
+    #from bokeh.io import output_file, show
+    #import pandas
+#preparing data
+    #df = pandas.read_csv("data.csv")
+    #x = df["x"]
+    #y = df["y"]
+#preparing the output file
+    #output_file("Line_from_csv.html")
+#creating figure object
+    #f = figure()
+#creating line plot
+    #f.line(x,y)
+#writing the plot in the figure object
+    #show(f)
+#This takes in data from a csv file and uses that data to make a plot line.
+
