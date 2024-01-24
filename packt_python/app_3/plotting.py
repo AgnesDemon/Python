dd7e498dc779a264093bb852250776f24c8bdc05
Chavez <chavelina0105@gmail.com>
2024-01-23 22:12:06 -0600

    Saving modified data

--- packt_python/app_3/plotting.py
+++ packt_python/app_3/plotting.py
@@ -3,12 +3,13 @@ from controlling_the_webcam_and_detecting_objects import df
 from bokeh.plotting import figure, show, output_file
 from bokeh.models import HoverTool, ColumnDataSource
 
-df["Start_string"] = df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
-df["End_string"] = df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
+df["Start_string"] = df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S") #The datetime values are converted to strings.
+#Those strings are then put in the df["Start"] column.
+df["End_string"] = df["End"].dt.strftime("%Y-%m-%d %H:%M:%S") #The datetime values are converted into strings and then placed in the df["End"] column.
 
-cds = ColumnDataSource(df)
+cds = ColumnDataSource(df) #This converts the df into a format that can be used by Bokeh for plotting.
 
-p = figure(x_axis_type = 'datetime', height = 100, width = 500, responsive = True, title = "Motion Graph")
+p = figure(x_axis_type = 'datetime', height = 100, width = 500, sizing_mode = "scale_width", title = "Motion Graph")
 p.yaxis.minor_tick_line_color = None
 p.ygrid[0].ticker.desired_num_ticks = 1
 
@@ -18,6 +19,7 @@ p.add_tools(hover)
 q = p.quad(left = "Start", right = "End", bottom = 0, top = 1, color = "green", source = cds)
 
 output_file("Start_time_graph.html")
+print("plotting.py data")
 show(p)
 
 
