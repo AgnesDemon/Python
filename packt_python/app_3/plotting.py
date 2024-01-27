eb806c1050af065955fd7223afb4f6e14d79f7ce
Chavez <chavelina0105@gmail.com>
2024-01-26 22:01:58 -0600

    Saving modified data

--- packt_python/app_3/plotting.py
+++ packt_python/app_3/plotting.py
@@ -1,8 +1,33 @@
 #This should access the data from the webcam and create a bar graph with that data that shows when motion is detected
-from controlling_the_webcam_and_detecting_objects import df
+#from controlling_the_webcam_and_detecting_objects import df
 from bokeh.plotting import figure, show, output_file
 from bokeh.models import HoverTool, ColumnDataSource
+import pandas
 
+df = pandas.read_csv("Times.csv")
+#print(df)
+df["Start_string"] = df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
+df["End_string"] = df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+cds = ColumnDataSource(df)
+
+p = figure(width=500, height=100, x_axis_type = 'datetime', sizing_mode = 'scale_width', title = "Motion Graph")
+p.yaxis.minor_tick_line_color = None
+p.ygrid[0].ticker.desired_num_ticks = 1
+hover = HoverTool(tooltips = [("Start", "@Start_string"), ("End", "@End_string")])
+p.add_tools(hover)
+
+q = p.quad(left = "Start", right = "End", bottom = 0, top = 1, color = "green", source = cds)
+
+output_file("start_time_graph.html")
+print("plotting.py data")
+show(p)
+
+
+
+
+
+#Original code from video:
 df["Start_string"] = df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S") #The datetime values are converted to strings.
 #Those strings are then put in the df["Start"] column.
 df["End_string"] = df["End"].dt.strftime("%Y-%m-%d %H:%M:%S") #The datetime values are converted into strings and then placed in the df["End"] column.
@@ -24,6 +49,3 @@ q = p.quad(left = "Start", right = "End", bottom = 0, top = 1, color = "green", 
 output_file("Start_time_graph.html")
 print("plotting.py data")
 show(p)
-
-
-
