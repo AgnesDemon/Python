b9e9d8e9d64341a4a3300ad6ed27bde37ec95bd1
Chavez <chavelina0105@gmail.com>
2024-02-02 12:53:18 -0600

    Saving modified data

---
+++ packt_python/app_4/data_analysis_and_visualization_with_pandas_and_matplotlib.py
@@ -0,0 +1,5 @@
+#For Jupyter notebook, type Ctrl + Enter to execute code on Windows.
+#This will execute code without creating a new box.
+#Shift + Enter will execute code and create a new box.
+#Note: had to install matplotlib by using pip install.
+
