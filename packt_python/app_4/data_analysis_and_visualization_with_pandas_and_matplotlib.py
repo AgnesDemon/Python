14587d21e9647c39f99dc639f9984cb0201d1083
Chavez <chavelina0105@gmail.com>
2024-02-05 19:07:53 -0600

    Saving modified data

--- packt_python/app_4/data_analysis_and_visualization_with_pandas_and_matplotlib.py
+++ packt_python/app_4/data_analysis_and_visualization_with_pandas_and_matplotlib.py
@@ -2,4 +2,10 @@
 #This will execute code without creating a new box.
 #Shift + Enter will execute code and create a new box.
 #Note: had to install matplotlib by using pip install.
-
+#In Jupyter Notebook, sometimes you have to run all of the cells (the boxes) again for it to work.
+#You can do this quickly by clicking Run and selecting Run All Cells.
+#You can create a new cell above all of the other cells by pressing A - just make sure you don't have any of the cells selected.
+#This should allow you to create a title if you type in 2 #s in the cell.
+#If you type 3 #s, then the font will be smaller.
+#You can also create a markdown cell by making sure nothing is selected and pressing M.
+#This should create a markdown cell, and you can do the same thing as you did with the title.
