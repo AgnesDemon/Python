c3204d98a396374871714552b9b141d68928daa1
Chavez <chavelina0105@gmail.com>
2024-03-07 13:28:24 -0600

    Saving modified data

--- packt_python/app_4/in_browser_interactive_plots.py
+++ packt_python/app_4/in_browser_interactive_plots.py
@@ -1,6 +1,6 @@
 import justpy as jp
 
-def app():
+'''def app():
     webpage = jp.QuasarPage() #creates the webpage
     heading1 = jp.QDiv(a=webpage, text="Analysis of Course Reviews", classes="text-h1 text-center") #makes heading more modern, gives it a bigger font and centers it
     #to find fonts, styles, etc, search "quasar style" and click on the "Stylus Variables | Quasar Framework" webpage
@@ -11,5 +11,14 @@ def app():
 
 jp.justpy(app) #calls app() function
 #justpy() is a function that takes care of calling other functions
-#when making updates to the code, you will have to stop it from running in the terminal by using Ctrl + C
+#when making updates to the code, you will have to stop it from running in the terminal by using Ctrl + C'''
 
+#Demo
+import justpy as jp
+
+def hello_world():
+    wp = jp.WebPage()
+    jp.Hello(a=wp)
+    return wp
+
+jp.justpy(hello_world)
