ab1b8e7256de968f4486edc72838aee22278f894
Chavez <chavelina0105@gmail.com>
2024-04-19 12:52:38 -0500

    Saving modified data

--- packt_python/graphical_UI.py
+++ packt_python/graphical_UI.py
@@ -4,7 +4,11 @@ from tkinter import *
 #a tkinter program is made up of widgets and the window
 window=Tk()
 
-b1 = Button(window, text="Execute") #designs the button (puts the word "Execute" on the button)
+def km_to_miles():
+    miles = float(e1_value.get())*1.6
+    t1.insert(END, miles)
+
+b1 = Button(window, text="Execute", command=km_to_miles) #designs the button (puts the word "Execute" on the button)
 #b1.pack() #creates the button
 b1.grid(row=0, column=0) #this method creates the button and places in a specific place
 #to see the parameters for the Button() function, do this:
@@ -14,10 +18,11 @@ b1.grid(row=0, column=0) #this method creates the button and places in a specifi
     #now type "Button?"
     #this should show you all the parameters for Button()
 
-e1=Entry(window) #creates an area where you can type (like a search bar)
+e1_value=StringVar()
+e1=Entry(window, textvariable=e1_value) #creates an area where you can type (like a search bar)
 e1.grid(row=0, column=1) #puts the search bar in a specific place
 
-t1=Text(window, height=5, width=20)
+t1=Text(window, height=1, width=20) #this creates a text window
 t1.grid(row=0, column=2)
 
 
