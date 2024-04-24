98b5f0da91800d1cbd56aa2ab6b8e6af8e76e8cb
Chavez <chavelina0105@gmail.com>
2024-04-24 13:02:50 -0500

    Saving modified data

--- packt_python/interacting_with_databases/interacting_w_databases.py
+++ packt_python/interacting_with_databases/interacting_w_databases.py
@@ -1,29 +1,49 @@
 import sqlite3
 
-def create_table():
-    connection =sqlite3.connect("lite.db") #connects to database
-    cursor = connection.cursor() #creates the cursor
-    cursor.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)") #will create a table if one does not already exist
-    cursor.execute("INSERT INTO store VALUES ('Wine Glass', 8, 10.5)")
-    connection.commit() #commits changes to the database
-    connection.close() #closes the connection to the database
+#connection = sqlite3.connect("lite.db")
+#cursor = connection.cursor()
+#cursor.execute("CREATE TABLE store (item TEXT, quantity INTEGER, price REAL)")
+#connection.commit()
+#connection.close()
 
-def insert(item, quantity, price):
-    connection =sqlite3.connect("lite.db")
+def create_table():
+    connection = sqlite3.connect("lite.db")
     cursor = connection.cursor()
-    cursor.execute("INSERT INTO store VALUES(?, ?, ?)", (item, quantity, price))
+    cursor.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
     connection.commit()
     connection.close()
 
-insert("Coffee Cup", 10, 5)
+def insert(item, quantity, price):
+    connection = sqlite3.connect("lite.db")
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO store VALUES (?,?,?)", (item, quantity, price))
+    connection.commit()
+    connection.close()
 
+#insert("Coffee Cup",8,10.5)
 
 def view():
-    connection =sqlite3.connect("lite.db")
+    connection = sqlite3.connect("lite.db")
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM store")
     rows = cursor.fetchall()
     connection.close()
     return rows
 
+def delete(item):
+    connection = sqlite3.connect("lite.db")
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM store WHERE item=?", (item,))
+    connection.commit()
+    connection.close()
+
+def update(quantity, price, item):
+    connection = sqlite3.connect("lite.db")
+    cursor = connection.cursor()
+    cursor.execute("UPDATE store SET quantity=?, price=? WHERE item=?", (quantity, price, item))
+    connection.commit()
+    connection.close()
+
+update(11, 6, "Water Glass")
+#delete("Wine Bottle")
 print(view())
