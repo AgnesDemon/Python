603ef9964fb15176f84aaa4f9faa2865cf93c7d1
Chavez <chavelina0105@gmail.com>
2024-05-02 13:14:34 -0500

    Saving modified data

---
+++ packt_python/app_6/book_inventory_UI.py
@@ -0,0 +1,40 @@
+from tkinter import *
+
+window = Tk()
+
+button1 = Button(window, text="View All")
+button1.grid(row=2, column=3)
+button2 = Button(window, text="Search Entry")
+button2.grid(row=3, column=3)
+button3 = Button(window, text="Add Entry")
+button3.grid(row=4, column=3)
+button4 = Button(window, text="Update Selected")
+button4.grid(row=5, column=3)
+button5 = Button(window, text="Delete Selected")
+button5.grid(row=6, column=3)
+button6 = Button(window, text="Close")
+button6.grid(row=7, column=3)
+
+entry_value = StringVar()
+entry1 = Entry(window, textvariable=entry_value)
+entry1.grid(row=0, column=1)
+entry2 = Entry(window, textvariable=entry_value)
+entry2.grid(row=0, column=3)
+entry3 = Entry(window, textvariable=entry_value)
+entry3.grid(row=1, column=1)
+entry4 = Entry(window, textvariable=entry_value)
+entry4.grid(row=1, column=3)
+
+label1 = Label(window, text="Title")
+label1.grid(row=0, column=0)
+label2 = Label(window, text="Author")
+label2.grid(row=0, column=2)
+label3 = Label(window, text="Year")
+label3.grid(row=1, column=0)
+label4 = Label(window, text="ISBN")
+label4.grid(row=1, column=2)
+
+text = Text(window, height=4, width=20)
+text.grid(row=3, column=0)
+
+window.mainloop()
