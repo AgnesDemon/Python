5e3277cc604c69ba17cea84789ccab0ef74dda7c
Chavez <chavelina0105@gmail.com>
2024-05-07 13:16:39 -0500

    Saving modified data

---
+++ packt_python/app_6/backend.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+def connect():
+    connection = sqlite3.connect("books.db")
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+    connection.commit()
+    connection.close()
+
+def insert(title, author, year, isbn):
+    connection = sqlite3.connect("books.db")
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO book VALUES (NULL,?,?,?,?)", (title,author,year,isbn))
+    connection.commit()
+    connection.close()
+
+def view():
+    connection = sqlite3.connect("books.db")
+    cursor = connection.cursor()
+    cursor.execute("sELECT * FROM  book")
+    rows = cursor.fetchall()
+    connection.close()
+    return rows
+
+def search(title="",author="",year="",isbn=""): #passing empty strings as default, which prevents an error
+    connection = sqlite3.connect("books.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM book WHERE title=?, OR author=? OR year=? OR isbn=?", (title,author,year,isbn))
+    rows = cursor.fetchall()
+    connection.close()
+    return rows
+
+
+connect()
+insert("Oliver Twist","Charles Dickens",1837,123123123)
+print(view())
