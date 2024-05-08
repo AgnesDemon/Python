aede1c03090a9cf0ccdd0e23f95d726e84cd1b53
Chavez <chavelina0105@gmail.com>
2024-05-08 13:02:16 -0500

    Saving modified data

--- packt_python/app_6/backend.py
+++ packt_python/app_6/backend.py
@@ -17,7 +17,7 @@ def insert(title, author, year, isbn):
 def view():
     connection = sqlite3.connect("books.db")
     cursor = connection.cursor()
-    cursor.execute("sELECT * FROM  book")
+    cursor.execute("SELECT * FROM  book")
     rows = cursor.fetchall()
     connection.close()
     return rows
@@ -25,12 +25,29 @@ def view():
 def search(title="",author="",year="",isbn=""): #passing empty strings as default, which prevents an error
     connection = sqlite3.connect("books.db")
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM book WHERE title=?, OR author=? OR year=? OR isbn=?", (title,author,year,isbn))
+    cursor.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title,author,year,isbn))
     rows = cursor.fetchall()
     connection.close()
     return rows
 
+def delete(id):
+    connection = sqlite3.connect("books.db")
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM book WHERE id=?", (id,))
+    connection.commit()
+    connection.close()
+
+def update(id,title,author,year,isbn):
+    connection = sqlite3.connect("books.db")
+    cursor = connection.cursor()
+    cursor.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?", (title,author,year,isbn,id))
+    connection.commit()
+    connection.close()
+
 
 connect()
-insert("Oliver Twist","Charles Dickens",1837,123123123)
+#update(3,"Little Women","Louisa May Alcott",1868,123456789)
+#delete(1)
+insert("Oliver Twist","Charles Dickens",1837,124124124)
 print(view())
+#print(search(year=1868))
