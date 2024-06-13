c593b01a23607efb3775c7444daa3c1f7cf37b49
Chavez <chavelina0105@gmail.com>
2024-06-13 13:10:36 -0500

    Saving modified data

--- Manga Organizer/MO_login.py
+++ Manga Organizer/MO_login.py
@@ -42,24 +42,38 @@ import tkinter
 window = tkinter.Tk()
 
 window.title("Login") #name of window
+window.geometry('340x440')
 window.configure(bg="#9F9F9F") #sets background color
 
+def login():
+    username = "AgnesDemon13"
+    password = "11004488"
+    if entry1.get() == username and entry2.get() == password:
+        print("Welcome, " + username)
+        window.destroy()
+    else:
+        print("Login failed. Please try again.")
+
+frame =tkinter.Frame(bg="#9F9F9F")
+
 #Labels
-label1 = tkinter.Label(window, text="Login", bg="#9F9F9F", fg="#FFFFFF", font=("Arial", 30))
-label1.grid(row=0, column=0, columnspan=2)
-label2 = tkinter.Label(window, text="Username", bg="#9F9F9F", fg="#FFFFFF", font=("Arial", 20))
+label1 = tkinter.Label(frame, text="Login", bg="#9F9F9F", fg="#FFFFFF", font=("Arial", 30))
+label1.grid(row=0, column=0, columnspan=2, sticky="news", pady=40) #'sticky=' tells label to take up as much space as possible in said directions 
+label2 = tkinter.Label(frame, text="Username", bg="#9F9F9F", fg="#FFFFFF", font=("Arial", 16))
 label2.grid(row=1, column=0)
-label3 = tkinter.Label(window, text="Password", bg="#9F9F9F", fg="#FFFFFF", font=("Arial", 20))
+label3 = tkinter.Label(frame, text="Password", bg="#9F9F9F", fg="#FFFFFF", font=("Arial", 16))
 label3.grid(row=2, column=0)
 
 #Entries
-entry1 = tkinter.Entry(window)
-entry1.grid(row=1, column=1)
-entry2 = tkinter.Entry(window, show="*")
-entry2.grid(row=2, column=1)
+entry1 = tkinter.Entry(frame, font=("Arial", 16))
+entry1.grid(row=1, column=1, pady=20)
+entry2 = tkinter.Entry(frame, show="*", font=("Arial", 16))
+entry2.grid(row=2, column=1, pady=20)
 
 #Button
-button = tkinter.Button(window, text="Login", bg="#404040", fg="#FFFFFF", command=window.destroy)
-button.grid(row=3, column=0, columnspan=2)
+button = tkinter.Button(frame, text="Login", bg="#404040", fg="#FFFFFF", font=("Arial", 16), command=login)
+button.grid(row=3, column=0, columnspan=2, pady=30)
+
+frame.pack()
 
 window.mainloop()
