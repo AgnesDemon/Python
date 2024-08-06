4e46e98d6187b40e809c6991c0449b9ef4b339c5
Chavez <chavelina0105@gmail.com>
2024-08-06 17:34:48 -0500

    Saving modified data

--- minigame.py
+++ minigame.py
@@ -3,19 +3,19 @@
 #Endings I could have:
     #Beginning of the game - You could decline the invitation to go to Japan with your friends. Therefore, nothing really happens.
     #Optional marbles -You accept the invitation to Japan and go with your friends. While exploring, you meet a vendor who wants to sell you marbles. You can either decline or accept the marbles. Your choice will affect the story later.
-    #Shortcut or long way around - You are in an area and want to get someplace. One friend suggests taking the short but eerie path. Another friend suggests taking the longer but seemingly safer path.
-    #-Shortcut: You take the eerie shortcut. Thankfully, nothing happens and you make it to your destination.
-    #Shortcut ending: You take the shortcut and make it to your destination. However, while you are watching TV later, you find someone was murdered in the area of the long path. You realize that the victim could have been you or your friends.
-    #Long path: You take the longer but safer path. There are more people around, and you bump into a strange woman wearing a large mask.
-    #The woman asks you if you think she is pretty/beautiful/attractive. You are given three choices: Yes, No, and Average.
-    #Average ending: You tell the woman she is average. She stares at you confused, then leaves.
-    #No: You tell her she isn’t pretty. She will pull out bloody scissors and attempt to stab you.
-    #-If you had bought the marbles, you can choose whether or not to throw them at her. If you don’t, you die. If you do, she will become distracted, giving you the time to run. This results in a trauma ending.
-    #Yes: You tell the woman she is pretty. The woman seems to gleam under her mask. Slowly, she removes it, revealing something horrifying underneath. She will ask, “How about now?”.
-    #-Scream: she will kill you unless you throw the marbles at her (if you have them). Endings: death or trauma.
-    #-Yes: She will grin evilly and disfigure your face, just like hers, unless you throw the marbles at her (if you have them). Endings: disfigured face or trauma.
-    #Trauma ending explains how you go back home from Japan, feeling paranoid all the time and never wanting to venture out of the house.
-    #Disfigured ending explains how what the woman did left you scarred forever. You didn’t have enough money for a surgery, so you have to wear a mask for the rest of your life. Just like her.
+    #You are in an area and want to get someplace. One friend suggests taking the short but eerie path. Another friend suggests taking the longer but seemingly safer path.
+        #Shortcut: You take the eerie shortcut. Thankfully, nothing happens and you make it to your destination.
+            #Shortcut ending: You take the shortcut and make it to your destination. However, while you are watching TV later, you find someone was murdered in the area of the long path. You realize that the victim could have been you or your friends.
+        #Long path: You take the longer but safer path. There are more people around, and you bump into a strange woman wearing a large mask.
+            #The woman asks you if you think she is pretty/beautiful/attractive. You are given three choices: Yes, No, and Average.
+                #Average ending: You tell the woman she is average. She stares at you confused, then leaves.
+                #No: You tell her she isn’t pretty. She will pull out bloody scissors and attempt to stab you.
+                    #If you had bought the marbles, you can choose whether or not to throw them at her. If you don’t, you die. If you do, she will become distracted, giving you the time to run. This results in a trauma ending.
+                #Yes: You tell the woman she is pretty. The woman seems to gleam under her mask. Slowly, she removes it, revealing something horrifying underneath. She will ask, “How about now?”.
+                    #Scream: she will kill you unless you throw the marbles at her (if you have them). Endings: death or trauma.
+                    #Yes: She will grin evilly and disfigure your face, just like hers, unless you throw the marbles at her (if you have them). Endings: disfigured face or trauma.
+                        #Trauma ending explains how you go back home from Japan, feeling paranoid all the time and never wanting to venture out of the house.
+                        #Disfigured ending explains how what the woman did left you scarred forever. You didn’t have enough money for a surgery, so you have to wear a mask for the rest of your life. Just like her.
 
 import os
 import time
@@ -26,6 +26,8 @@ time.sleep(3)
 clear()
 
 class Minigame():
+    marbles = []
+
     def first_choice(this):
         choice1 = input("Your friends want to take a spontaneous trip to Japan. Do you want to go?\n")
         if choice1 == "yes" or "Yes":
