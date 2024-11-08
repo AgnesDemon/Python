7b935136290116f642a314a3ccd530cdee30df5a
Chavez <chavelina0105@gmail.com>
2024-11-08 12:24:07 -0600

    Saving modified data

--- packt_python/app_10/app.py
+++ packt_python/app_10/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_file
+import pandas
 
 app = Flask(__name__)
 
@@ -8,7 +9,13 @@ def home():
 
 @app.route("/geocoder/")
 def geocoder():
+    #csv_file = pandas.read_csv("test_file.csv")
     return render_template("geocoder.html")
+    #return csv_file.to_html()
+
+@app.route("/success_table", methods=["POST"])
+def success_table():
+    return render_template
 
 if __name__ == "__main__":
     app.run(debug=True)
