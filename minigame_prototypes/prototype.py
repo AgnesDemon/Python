27be2a693e7184eb5781fe97c0bb6d6fe56a8c0b
Chavez <chavelina0105@gmail.com>
2024-12-06 13:09:49 -0600

    Saving modified data

---
+++ minigame_prototypes/prototype.py
@@ -0,0 +1,25 @@
+import pandas
+import os
+clear = lambda: os.system('cls')
+
+#PRINTS OUT ALL LINES AT THE SAME TIME IN THEIR OWN LINE, NOT WHAT I WANT
+'''with open("sampletext.txt", "r") as file:
+    for line in file:
+        print(line.strip())'''
+
+#PRINTS OUT ALL LINES AT THE SAME TIME IN THEIR OWN LINE, THIS TIME SHOWING THEIR LINE NUMBER, NOT WHAT I WANT
+'''file = open('sampletext.txt', 'r')
+count = 0
+for line in file:
+    count += 1
+    print("Line {}: {}".format(count, line.strip()))
+file.close()'''
+
+#CAN PRINT LINES ONE AT A TIME, THIS ONE MAY WORK
+with open('sampletext.txt', 'r') as file:
+    line = file.readline()
+    while line:
+        #print(line.strip())
+        clear()
+        input(line.strip())
+        line = file.readline()
