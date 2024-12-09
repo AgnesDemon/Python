dd267b00633727f1f30b5d3f05c4e216b5e9876d
Chavez <chavelina0105@gmail.com>
2024-12-09 16:25:48 -0600

    Saving modified data

--- minigame_prototypes/app.py
+++ minigame_prototypes/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pandas
 import os
+clear = lambda: os.system('cls')
 
 app = Flask(__name__)
 
@@ -10,14 +11,20 @@ app.config['UPLOAD_FOLDER'] = image_folder
 
 @app.route('/')
 def pageone():
-    img_1 = os.path.join(app.config['UPLOAD_FOLDER'], 'img.png')
-    return render_template("pageone.html", user_image = img_1)
+    #img_1 = os.path.join(app.config['UPLOAD_FOLDER'], 'img.png')
+    with open('sampletext.txt', 'r') as file:
+        filename = file.read()
+    #return render_template("pageone.html", user_image = img_1, content=filename)
+    return render_template("pageone.html", content=filename)
 
 @app.route('/secondpage/')
 def pagetwo():
     #img_2 = os.path.join(app.config['UPLOAD_FOLDER'], 'img2.png')
     #return render_template("pagetwo.html", user_image = img_2)
-    return render_template("pagetwo.html")
+    with open('sampletext.txt', 'r') as file:
+        #filename = file.read() #reads all text lines next to each other
+        filename = file.readline() #reads only the first line of text file
+    return render_template("pagetwo.html", content=filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
