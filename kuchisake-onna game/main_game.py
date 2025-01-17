0cd728d26fb68aed731c0916f7ee3e581cbcc540
Chavez <chavelina0105@gmail.com>
2025-01-17 13:11:36 -0600

    Saving modified data

--- kuchisake-onna game/main_game.py
+++ kuchisake-onna game/main_game.py
@@ -14,11 +14,30 @@ print(inventory)
 lose_item = inventory - 1
 print(lose_item)
 
-def handle_shutdown():
+@app.route("/", methods = ["GET", "POST"])
+def start():
+    text = {}
+    index = 0
+    with open("text_set_one.txt") as file:
+        for line in file:
+            index = index + 1
+            text[index] = line.strip()
+    if request.method == "POST":
+        try:
+            line_num = request.form.get('current_line')
+            num = int(line_num) + 1
+            return render_template("start.html", content = text[num], line_number = num)
+        except:
+            text = "Should you go on the trip? Type 'yes' or 'no' for your answer."
+            return render_template("start.html", content = text)
+    return render_template("start.html", content = text[1], line_number = 1)
+
+
+'''def handle_shutdown():
     print("Shutting down Flask app")
     os._exit(0)
 
-signal.signal(signal.SIGINT, handle_shutdown)
+signal.signal(signal.SIGINT, handle_shutdown)'''
 
 if __name__ == "__main__": #runs app
     app.run(debug=True)
