971af7efb00a77e8fb48585a36c173298ce2c15a
Chavez <chavelina0105@gmail.com>
2025-01-31 12:18:11 -0600

    Saving modified data

--- kuchisake-onna game/main_game.py
+++ kuchisake-onna game/main_game.py
@@ -16,7 +16,7 @@ inventory = 0
 def start():
     text = {}
     index = 0
-    with open("text_set_one.txt") as file:
+    with open("text_set_one.txt", 'r') as file:
         for line in file:
             index = index + 1
             text[index] = line.strip()
@@ -32,7 +32,7 @@ def start():
             input = request.form.get('input')
             if input == "yes":
                 secondpart()
-                return render_template("second_part.html")
+                #return render_template("second_part.html")
             elif input == "no":
                 ending_line = "You decide that it's not the best idea. Your friends are disappointed, but they understand. \nEnding: You don't go on the trip."
                 return render_template("start.html", content = ending_line)
@@ -42,28 +42,32 @@ def start():
 
 @app.route("/secondpart", methods = ["GET", "POST"])
 def secondpart():
-    text = {}
-    index = 0
-    with open("text_set_two.txt") as file:
+    text2 = {}
+    index2 = 0
+    print("It is reaching this part.")
+    with open("text_set_two.txt", 'r') as file:
         for line in file:
-            index = index + 1
-            text[index] = line.strip()
+            index2 = index2 + 1
+            text2[index2] = line.strip()
     if request.method == "POST":
         try:
-            line_num = request.form.get('current_line')
-            num = int(line_num) + 1
-            return render_template("second_part.html", content = text[num], line_number = num)
+            line_num2 = request.form.get('current_line')
+            num2 = int(line_num2) + 1
+            print("It is reaching the try method.")
+            return render_template("second_part.html", content = text2[num2], line_number = num2)
         except:
+            #print("It is reaching this line.")
             text = "Should you buy the candy? Type 'yes' or 'no' for your answer."
+            print("It is reaching the except method.")
             if request.method == "GET":
                 return render_template("question_page.html", content = text)
-            #input = request.form.get('input')
-            #if input == 'yes':
+            input = request.form.get('input')
+            if input == 'yes':
                 #inventory += 1
                 #print(inventory)
-                #marbles = "You bought the marbles"
-                #return render_template("question_page.html", content = marbles, text = "You bought the marbles." )
-    return render_template("second_part.html", content = text[1], line_number = 1)
+                marbles = "You bought the marbles"
+                return render_template("secondpage.html", content = marbles, text = "You bought the marbles." )
+    return render_template("second_part.html", content = text2[1], line_number = 1)
 
 
 '''def handle_shutdown():
