a2409bf102c946abf105db8f758a7d3c5e4335d7
Chavez <chavelina0105@gmail.com>
2025-02-13 13:03:02 -0600

    Saving modified data

--- kuchisake-onna game/main_game.py
+++ kuchisake-onna game/main_game.py
@@ -32,18 +32,19 @@ class KuchisakeOnna:
                 return render_template("start.html", content = text[num], line_number = num)
             except:
                 text = "Should you go on the trip? Type 'yes' or 'no' for your answer."
-                if request.method == "GET":
+                return render_template("first_choice.html")
+                '''if request.method == "GET":
                     return render_template("question_page.html", content = text)
                 input = request.form.get('input')
                 if input == "yes":
                     file.close()
                     KuchisakeOnna.secondpart()
-                    return render_template("second_part.html")
+                    return render_template("second_part.html", content = KuchisakeOnna.secondpart.text2[1], line_number = 1)
                 elif input == "no":
                     ending_line = "You decide that it's not the best idea. Your friends are disappointed, but they understand. \nEnding: You don't go on the trip."
                     return render_template("start.html", content = ending_line)
                 else:
-                    return render_template("question_page.html", content = text, text = "Please type in a valid response.")
+                    return render_template("question_page.html", content = text, text = "Please type in a valid response.")'''
         return render_template("start.html", content = text[1], line_number = 1)
 
     @app.route("/secondpart", methods = ["GET", "POST"])
